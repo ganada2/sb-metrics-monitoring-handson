fa623a2f437b5b0b5690917cf3cf3dd9c04ab84b
mangdan <dan.donghu.kim@gmail.com>
2019-06-11 23:04:12 +0900

    2019-06-11 23:04:12

--- start-all-svc.sh
+++
@@ -1,3 +0,0 @@
-nohup mvn spring-boot:run -f love-calculator-service &
-nohup mvn spring-boot:run -f love-calculator-consumer &
-nohup mvn spring-boot:run -f yes-or-no-consumer &
--- stop-all-svc.sh
+++
@@ -1,3 +0,0 @@
-kill $(cat ./love-calculator-service/shutdown.pid)
-kill $(cat ./love-calculator-consumer/shutdown.pid)
-kill $(cat ./yes-or-no-consumer/shutdown.pid)
